b0f8bec44526782916876f7da26ed8f59380312e
RSLastra <raul.slastra@gmail.com>
2024-11-24 22:56:42 -0600

    feat: simple pagination

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -3,7 +3,20 @@ import prisma from '@/app/lib/prisma';
 
 export async function GET(request: Request) {
 
-  const todos = await prisma.todo.findMany();
+  const { searchParams } = new URL(request.url);
+  const take = Number(searchParams.get('take') ?? '10');
+  const skip = Number(searchParams.get('skip') ?? '0');
+
+  if (isNaN(take)) return NextResponse.json(
+    { message: 'take tiene que ser un numero' },
+    { status: 400 }
+  )
+  if (isNaN(skip)) return NextResponse.json(
+    { message: 'skip tiene que ser un numero' },
+    { status: 400 }
+  )
+
+  const todos = await prisma.todo.findMany({ take, skip });
 
   return NextResponse.json(todos);
 }
