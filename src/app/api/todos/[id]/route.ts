534ee70bbd0b7886aa1218ef7ca46fce96e1759c
RSLastra <raul.slastra@gmail.com>
2024-11-25 22:19:27 -0600

    feat: validate put with yup

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/app/lib/prisma';
 import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup';
 
 interface Segments {
   params: {
@@ -17,23 +18,35 @@ export async function GET(request: Request, { params }: Segments) {
     return NextResponse.json({ message: `Tdoo con id: ${id} no encontrado` }, { status: 404 })
   }
 
-  return NextResponse.json(todo)
+  return NextResponse.json(todo);
 }
+
+const putSchema = yup.object({
+  description: yup.string().optional(),
+  complete: yup.boolean().optional(),
+})
+
 export async function PUT(request: Request, { params }: Segments) {
 
   const { id } = await params;
-  const body = await request.json();
+  const { complete, description } = await putSchema.validate(await request.json());
 
-  const todo = await prisma.todo.findFirst({ where: { id } });
+  try {
+    const todo = await prisma.todo.findFirst({ where: { id } });
 
-  if (!todo) {
-    return NextResponse.json({ message: `Tdoo con id: ${id} no encontrado` }, { status: 404 })
+    if (!todo) {
+      return NextResponse.json({ message: `Tdoo con id: ${id} no encontrado` }, { status: 404 })
+    }
+
+    const updatedTodo = await prisma.todo.update({
+      where: { id },
+      data: { complete, description }
+    })
+
+    return NextResponse.json(updatedTodo)
+
+  } catch (error) {
+
+    return NextResponse.json(error, { status: 400 })
   }
-
-  const updatedTodo = await prisma.todo.update({
-    where: { id },
-    data: body
-  })
-
-  return NextResponse.json(updatedTodo)
 }
