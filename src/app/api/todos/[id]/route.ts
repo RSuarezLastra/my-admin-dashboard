45de9b6519348179c3d744aefe32065dabdc6596
RSLastra <raul.slastra@gmail.com>
2024-11-25 21:55:10 -0600

    feat: put todo function

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -19,3 +19,21 @@ export async function GET(request: Request, { params }: Segments) {
 
   return NextResponse.json(todo)
 }
+export async function PUT(request: Request, { params }: Segments) {
+
+  const { id } = await params;
+  const body = await request.json();
+
+  const todo = await prisma.todo.findFirst({ where: { id } });
+
+  if (!todo) {
+    return NextResponse.json({ message: `Tdoo con id: ${id} no encontrado` }, { status: 404 })
+  }
+
+  const updatedTodo = await prisma.todo.update({
+    where: { id },
+    data: body
+  })
+
+  return NextResponse.json(updatedTodo)
+}
