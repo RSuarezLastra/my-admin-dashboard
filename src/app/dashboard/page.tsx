517273c8562dbcc5a20d56268ed9e0217645a744
RSLastra <raul.slastra@gmail.com>
2024-11-28 21:36:05 -0600

    feat: widgetitem

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,12 @@
+import { WidgetItem } from '../../components/WidgetItem';
 
 export default function DashboardPage() {
   return (
-    <div>
-      <h1>Page dashboard</h1>
-    </div>
+
+    < div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3" >
+
+      <WidgetItem />
+
+    </div >
   );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { CiBookmarkCheck, CiLogout } from 'react-icons/ci';
+import { CiLogout } from 'react-icons/ci';
 import { SidebarItem } from './SidebarItem';
 
 const navLinks = [
