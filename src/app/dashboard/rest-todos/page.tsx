d65299b17da175baf0bf45ed574d1d574d5fc28f
RSLastra <raul.slastra@gmail.com>
2024-11-28 22:32:46 -0600

    feat: TodosGrid and TodoItem

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,8 +1,21 @@
+import prisma from "@/app/lib/prisma";
+import { TodosGrid } from "@/todos";
 
-export default function RestTodosPage() {
+export const metadata = {
+  title: 'Rest todo page',
+  description: 'descripcion'
+}
+
+export default async function RestTodosPage() {
+
+  const todos = await prisma.todo.findMany();
+
   return (
     <div>
       <h1>Rest Todos Page</h1>
+      {
+        <TodosGrid todos={todos} />
+      }
     </div>
   );
 }
