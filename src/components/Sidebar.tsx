9cf2af7f0776bec8e0317e33b8bd7a392026e13e
RSLastra <raul.slastra@gmail.com>
2024-11-28 22:03:53 -0600

    feat: active link

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,10 +2,24 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { CiLogout } from 'react-icons/ci';
 import { SidebarItem } from './SidebarItem';
+import { IoCalendarOutline, IoCheckboxOutline, IoListOutline } from 'react-icons/io5';
 
 const navLinks = [
-  { title: 'Dashboard', path: '#' },
-  { title: 'Categories', path: '#' },
+  {
+    title: 'Dashboard',
+    path: '/dashboard',
+    icon: <IoCalendarOutline />
+  },
+  { 
+    title: 'Rest Todos', 
+    path: '/dashboard/rest-todos',
+    icon: <IoCheckboxOutline/>
+  },
+  { 
+    title: 'Server Actions', 
+    path: '/dashboard/server-actions',
+    icon: <IoListOutline/>
+  },
 ]
 
 export const Sidebar = () => {
@@ -14,18 +28,28 @@ export const Sidebar = () => {
       <div>
         <div className="-mx-6 px-6 py-4">
           <Link href="/dashboard" title="home">
-            <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7RDEtPGvqNOxsei62fAUnKqBZkR5tyrOilA&s" width={100} height={50} alt="tailus logo" />
+            <Image
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7RDEtPGvqNOxsei62fAUnKqBZkR5tyrOilA&s"
+              width={100}
+              height={50}
+              alt="tailus logo"
+            />
           </Link>
         </div>
 
         <div className="mt-8 text-center">
-          <Image src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp" alt="" width={100} height={50} className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28" />
+          <Image
+            src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
+            alt="user image"
+            width={100}
+            height={50}
+            className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
+          />
           <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">Cynthia J. Watts</h5>
           <span className="hidden text-gray-400 lg:block">Admin</span>
         </div>
 
         <ul className="space-y-2 tracking-wide mt-8">
-          {/* Active className: text-white bg-gradient-to-r from-sky-600 to-cyan-400 */}
           {
             navLinks.map(item => (
               <SidebarItem key={item.title} {...item} />
--- src/components/SidebarItem.tsx
+++ src/components/SidebarItem.tsx
@@ -1,17 +1,26 @@
+'use client'
+
 import Link from "next/link";
-import { CiBookmarkCheck } from 'react-icons/ci';
+import { usePathname } from "next/navigation";
+
 
 interface Props {
+  icon: React.ReactNode;
   title: string;
   path: string;
 }
 
-export const SidebarItem = ({ title, path }: Props) => {
+
+export const SidebarItem = ({ title, path, icon }: Props) => {
+
+  const pathName = usePathname();
+
   return (
     <li>
-      <Link href={path} className="relative px-4 py-3 flex items-center space-x-4 rounded-xl text-gray-600">
-        <CiBookmarkCheck size={30} />
-        <span className="group-hover:text-gray-700">{title}</span>
+      <Link href={path} className={`relative px-4 py-3 flex items-center space-x-4 rounded-xl hover:bg-sky-600 hover:text-white text-gray-600 group
+        ${path === pathName ? 'text-white bg-gradient-to-r from-sky-600 to-cyan-400' : ''}`}>
+        {icon}
+        <span className="group-hover:text-white-700">{title}</span>
       </Link>
     </li>
   )
