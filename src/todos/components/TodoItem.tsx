2c35ea7e9b2f1e91ea130ff002987209e88225bc
RSLastra <raul.slastra@gmail.com>
2024-11-29 22:44:37 -0600

    feat: update complete

--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -1,21 +1,26 @@
+'use client';
+
 import { Todo } from "@prisma/client";
 import style from './TodoItem.module.css';
 import { IoCheckboxOutline, IoSquareOutline } from "react-icons/io5";
 
 interface Props {
   todo: Todo;
+  toggleTodo: (id: string, complete: boolean) => Promise<Todo>
 }
 
-export const TodoItem = ({ todo }: Props) => {
+export const TodoItem = ({ todo, toggleTodo }: Props) => {
   return (
     <div className={todo.complete ? style.todoDone : style.todoPending}>
       <div className="flex flex-col sm:flex-row gap-4 justify-start items-center">
-        <div className={`felx p-2
-          ${todo.complete ? 'bg-blue-100' : 'bg-red-100' }  rounded-md hover:bg-opacity-60 cursor-pointer`} >
+        <div
+          onClick={() => toggleTodo(todo.id, !todo.complete)}
+          className={`felx p-2
+          ${todo.complete ? 'bg-blue-100' : 'bg-red-100'}  rounded-md hover:bg-opacity-60 cursor-pointer`} >
           {
-            todo.complete 
-            ? <IoCheckboxOutline size={30} />
-            : <IoSquareOutline size={30} />
+            todo.complete
+              ? <IoCheckboxOutline size={30} />
+              : <IoSquareOutline size={30} />
           }
         </div>
         <div className="text-center sm:text-left">
