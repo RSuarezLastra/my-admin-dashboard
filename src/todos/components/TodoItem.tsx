3267f0a4c904d2221b92aa298ffbb44ba9e9d153
RSLastra <raul.slastra@gmail.com>
2024-11-29 23:03:26 -0600

    feat: router reresh

--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -6,7 +6,7 @@ import { IoCheckboxOutline, IoSquareOutline } from "react-icons/io5";
 
 interface Props {
   todo: Todo;
-  toggleTodo: (id: string, complete: boolean) => Promise<Todo>
+  toggleTodo: (id: string, complete: boolean) => void
 }
 
 export const TodoItem = ({ todo, toggleTodo }: Props) => {
