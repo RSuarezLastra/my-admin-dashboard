5eaf36bdd393d3ea92a981e4494242166acfd04e
RSLastra <raul.slastra@gmail.com>
2024-12-08 12:03:09 -0600

    feat: add userId relation, update seed

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,17 +1,26 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/app/lib/prisma';
+import bcrypt from 'bcryptjs';
 
 export async function GET(request: Request) {
 
   await prisma.todo.deleteMany();
+  await prisma.user.deleteMany();
 
-  await prisma.todo.createMany({
-    data: [
-      { description: 'Ir al gym', complete: true },
-      { description: 'Estudiar curso de Next' },
-      { description: 'Leer libro de metafisica' },
-      { description: 'Estudiar algoritmos' },
-    ]
+  await prisma.user.create({
+    data: {
+      name: 'Test User',
+      email: 'user@test.com',
+      password: bcrypt.hashSync('123456'),
+      todos: {
+        create: [
+          { description: 'Ir al gym', complete: true },
+          { description: 'Estudiar curso de Next' },
+          { description: 'Leer libro de metafisica' },
+          { description: 'Estudiar algoritmos' },
+        ]
+      }
+    }
   })
 
   return NextResponse.json({
