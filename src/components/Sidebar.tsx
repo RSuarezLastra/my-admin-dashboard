a74938b9893b39dc0966a5451044203a5b0d1078
RSLastra <raul.slastra@gmail.com>
2024-12-07 18:39:22 -0600

    feat: AuthProvider get data session client side

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { getServerSession } from 'next-auth';
 import { CiLogout } from 'react-icons/ci';
 import { SidebarItem } from './SidebarItem';
-import { IoCalendarOutline, IoCartOutline, IoCheckboxOutline, IoCodeWorking, IoListOutline } from 'react-icons/io5';
+import { IoCalendarOutline, IoCartOutline, IoCheckboxOutline, IoCodeWorking, IoListOutline, IoPersonOutline } from 'react-icons/io5';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 const navLinks = [
@@ -32,6 +32,11 @@ const navLinks = [
     path: '/dashboard/products',
     icon: <IoCartOutline />
   },
+  {
+    title: 'Perfil',
+    path: '/dashboard/profile',
+    icon: <IoPersonOutline/>
+  },
 ]
 
 export const Sidebar = async () => {
@@ -42,11 +47,11 @@ export const Sidebar = async () => {
   const avatar = (session?.user?.image)
     ? session?.user?.image
     : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7RDEtPGvqNOxsei62fAUnKqBZkR5tyrOilA&s"
-    
+
   //TODO const role = session?.user?.role ?? 'User';
 
   return (
-    <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
+    <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%] overflow-y-auto">
       <div>
         <div className="-mx-6 px-6 py-4">
           <Link href="/dashboard" title="home">
